ace0355ecd836d9511a1a2e8fbbdb1400e957c4e
SlavaBotnarenku <botnarenkuslava@gmail.com>
2026-02-08 12:33:11 +0300

    solved seconds_to_str

--- tasks/seconds_to_str.py
+++ tasks/seconds_to_str.py
@@ -16,4 +16,25 @@ def seconds_to_str(seconds: int) -> str:
         >> seconds_to_str(93600)
         01d02h00m00s
     """
-    raise NotImplementedError
+    # there are 86400 sec in a day
+    seconds = seconds
+    minute = 60
+    hour = 60 * minute
+    day = hour * 24
+
+    days = seconds // day   
+    hours = (seconds - (days * day)) // hour
+    minutes = (seconds - ((days * day) + (hours * hour))) // minute
+    seconds_left = seconds % minute
+    
+    # part that prints result and formants it
+    if days:
+        return f'{days:02d}d{hours:02d}h{minutes:02d}m{seconds_left:02d}s'
+    elif hours:
+        return f'{hours:02d}h{minutes:02d}m{seconds_left:02d}s'
+    elif minutes:
+        return f'{minutes:02d}m{seconds_left:02d}s'
+    else:
+        return f'{seconds_left:02d}s'
+        
+print(seconds_to_str(5))
