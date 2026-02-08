0b2c631ce151d0b61cc5f2bbac861262d91e134c
SlavaBotnarenku <botnarenkuslava@gmail.com>
2026-02-08 20:01:51 +0300

    solved 3rd even_odd

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -1,5 +1,6 @@
 __all__ = ("even_odd",)
 
+lst = [1, 2, 3, 4, 5]
 
 def even_odd(numbers: list[int]) -> float:
     """Определяет отношение суммы четных элементов списка
@@ -9,4 +10,20 @@ def even_odd(numbers: list[int]) -> float:
         >> even_odd([1, 2, 3, 4, 5])
         0.6667
     """
-    raise NotImplementedError
+    def check_if_odd(num: int) -> bool:
+        return True if num % 2 == 0 else False
+    
+    even_lst = []
+    odd_lst = []
+    for i in numbers:
+        if check_if_odd(i):
+            odd_lst.append(i)
+        else:
+            even_lst.append(i)
+
+    if (sum(odd_lst) == 0) or (sum(even_lst) == 0):
+        return 0
+
+    return (sum(odd_lst) / sum(even_lst))
+
+print(even_odd(lst))
